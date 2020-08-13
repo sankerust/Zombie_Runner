e4702f3942602fe56dcd222f7baef00e780ebda7
sanke <egor.tyukin@gmail.com>
2020-08-13 11:16:42 +0300

    add attack range for enemy

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,16 +6,45 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
   [SerializeField] Transform target;
+  [SerializeField] float chaseRange = 5f;
   NavMeshAgent navMeshAgent;
-    // Start is called before the first frame update
+  float distanceToTarget = Mathf.Infinity;
+  bool isProvoked = false;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
-
-    // Update is called once per frame
+    void OnDrawGizmosSelected()
+    {
+      Gizmos.color = Color.red;
+      Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
     void Update()
     {
+      distanceToTarget = Vector3.Distance(target.position, transform.position);
+      if (distanceToTarget > chaseRange) {
+        isProvoked = false;
+      }
+      if (isProvoked) {
+        EngageTarget();
+      } else if (distanceToTarget <= chaseRange) {
+        isProvoked = true;
+      }
+    }
+
+    private void EngageTarget() {
+      if (distanceToTarget > navMeshAgent.stoppingDistance) {
+        ChaseTarget();
+      } 
+      if (distanceToTarget <= navMeshAgent.stoppingDistance) {
+        AttackTarget();
+      }
+    }
+
+    private void ChaseTarget() {
       navMeshAgent.SetDestination(target.position);
+    }
+    private void AttackTarget() {
+      print("attacking");
     }
 }
