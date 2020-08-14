295df466d81bc9c69774fae961483aa4de0d0178
sanke <egor.tyukin@gmail.com>
2020-08-14 14:26:05 +0300

    add animation states and animations add player health add enemy damage to player add UI add scene loader

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+  public float playerHitPoints = 100f;
+    public void DamagePlayer(float damage) {
+      if (playerHitPoints <= damage) {
+        print("U DED");
+      } else {
+        playerHitPoints -= damage;
+        print(playerHitPoints);
+      }
+    }
+}
