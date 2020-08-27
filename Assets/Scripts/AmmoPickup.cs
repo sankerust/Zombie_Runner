ca23d3e968b68bcbbe1b62e75997a502234fb475
sanke <egor.tyukin@gmail.com>
2020-08-27 19:06:25 +0300

    add ammo pickups

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+  [SerializeField] int ammoAmount = 5;
+  [SerializeField] AmmoType ammoType;
+private void OnTriggerEnter(Collider whoCollided)
+  {
+    if (whoCollided.tag == "Player") {
+      FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
+      Debug.Log("collided with a pickup");
+      Destroy(gameObject);
+    }
+  }
+}
