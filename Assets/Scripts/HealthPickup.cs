f0ac7150d55e8d4bd4e4d9a557700b2f026a8b72
sanke <egor.tyukin@gmail.com>
2020-10-17 17:53:59 +0300

    add first aid pickup

---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+  [SerializeField] int amountRestored = 25;
+  [SerializeField] AudioClip pickupSound;
+  AudioSource audioSource;
+  PlayerHealth playerHealth;
+  private void Start() {
+    playerHealth = FindObjectOfType<PlayerHealth>();
+    audioSource = GetComponent<AudioSource>();
+  }
+  private void OnTriggerEnter(Collider whoCollided)
+  {
+
+    if (whoCollided.tag == "Player")
+    {
+      if (playerHealth.playerHitPoints < 100) {
+        playerHealth.playerHitPoints += amountRestored;
+        AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+        if(playerHealth.playerHitPoints > 100) {
+          playerHealth.playerHitPoints = 100;
+        }
+        Destroy(gameObject);
+      }
+    }
+  }
+}
